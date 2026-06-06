d9136c11797c8d8100d1b83c9e7f6a2c3ec3d88f
nitaoktaviani28 <nitaoktaviani2005@gmail.com>
2026-06-06 14:39:28 +0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,11 +1,15 @@
 import numpy as np
-from miskibin import get_logger
-from logging import Logger
+from logging import Logger, getLogger
 from enum import Enum
 import os
 from copy import deepcopy
 from board import Board2048, Direction
 
+# Ganti miskibin get_logger dengan logging bawaan Python
+def get_logger(name: str) -> Logger:
+    import logging
+    logging.basicConfig(level=logging.INFO)
+    return logging.getLogger(name)
 
 class Engine2048:
     """
@@ -17,21 +21,16 @@ class Engine2048:
         - Monte Carlo Tree Search
         - Expectimax
     """
-
     def __init__(self, logger: Logger, board: Board2048 = Board2048()) -> None:
         self.logger = logger
         self.board = board
 
     def find_best_move(self, board: Board2048, depth=3) -> Direction:
-        """
-        This function uses the minimax algorithm to find the best move.
-        """
         try:
             self.board._board = board._board
         except AttributeError:
             self.logger.error("Board is not a Board2048 object")
             raise AttributeError
-
         best_score = -1
         best_move = None
         for direction in (
